e37cfa38ff6e5f22d90242722bdcd86c6ac16e7b
Edan Odongo <edanodongo05@gmail.com>
2026-02-25 20:42:28 +0300

    superadmin mixin

---
+++ core/mixins.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponseForbidden
+
+
+class SuperAdminRequiredMixin:
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return HttpResponseForbidden("Authentication required")
+
+        if not request.user.is_superadmin:
+            return HttpResponseForbidden("SuperAdmin access required")
+
+        return super().dispatch(request, *args, **kwargs)
