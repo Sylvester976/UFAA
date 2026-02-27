a10327659a90eff6ab0e7c09c20d77153ef75ccf
Sylvester976 <officialotienosylvester@gmail.com>
2026-02-28 00:36:47 +0300

    profile complete

--- recruitment/models.py
+++ recruitment/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from accounts.models import JobseekerAccount
 
 
 class Gender(models.Model):
@@ -73,3 +74,29 @@ class Ward(models.Model):
 
     def __str__(self):
         return f'{self.name} — {self.constituency.name}'
+
+class JobSeekerProfile(models.Model):
+    user                = models.OneToOneField(JobseekerAccount, on_delete=models.CASCADE, related_name='profile')
+    salutation          = models.CharField(max_length=50, blank=True, null=True)
+    surname             = models.CharField(max_length=255, blank=True, null=True)
+    first_name          = models.CharField(max_length=255, blank=True, null=True)
+    second_name         = models.CharField(max_length=255, blank=True, null=True)
+    email               = models.EmailField(blank=True, null=True)
+    id_no               = models.CharField(max_length=20, blank=True, null=True)
+    date_of_birth       = models.DateField(blank=True, null=True)
+    gender              = models.ForeignKey(Gender, on_delete=models.SET_NULL, null=True, blank=True)
+    ethnic_group        = models.ForeignKey(EthnicGroup, on_delete=models.SET_NULL, null=True, blank=True)
+    home_county         = models.ForeignKey(County, on_delete=models.SET_NULL, null=True, blank=True)
+    constituency        = models.ForeignKey(Constituency, on_delete=models.SET_NULL, null=True, blank=True)
+    sub_county          = models.ForeignKey(SubCounty, on_delete=models.SET_NULL, null=True, blank=True)
+    ward                = models.ForeignKey(Ward, on_delete=models.SET_NULL, null=True, blank=True)
+    disability_status   = models.CharField(max_length=50, blank=True, null=True)
+    date_created        = models.DateTimeField(auto_now_add=True)
+    date_updated        = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name        = 'Job Seeker Profile'
+        verbose_name_plural = 'Job Seeker Profiles'
+
+    def __str__(self):
+        return f'{self.user.name} — Profile'
