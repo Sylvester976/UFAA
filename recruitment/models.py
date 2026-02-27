1c2fcc84ad6e0537e325de8ab2e63d8167915751
Sylvester976 <officialotienosylvester@gmail.com>
2026-02-27 17:40:10 +0300

    end of day worshop week 1

--- recruitment/models.py
+++ recruitment/models.py
@@ -11,3 +11,14 @@ class Gender(models.Model):
 
     def __str__(self):
         return self.name
+
+class EthnicGroup(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name        = 'Ethnic Group'
+        verbose_name_plural = 'Ethnic Groups'
+        ordering            = ['name']
+
+    def __str__(self):
+        return self.name
