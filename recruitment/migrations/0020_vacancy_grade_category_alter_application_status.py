1b659af04d44e0f308d14369380de68871d7246c
Edan Odongo <edanodongo05@gmail.com>
2026-03-03 11:42:26 +0300

    vacancy migration update

--- recruitment/migrations/0020_vacancy_grade_category_alter_application_status.py
+++ recruitment/migrations/0020_vacancy_grade_category_alter_application_status.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('recruitment', '0017_populate_subcounties'),
+        ('recruitment', '0019_professionalbodymembership'),
     ]
 
     operations = [
